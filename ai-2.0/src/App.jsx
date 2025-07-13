b4e39b1b03f5be9b81a4c3d554d081a1122d9312
Nilesh Swain <nileshswain715@gmail.com>
2025-07-13 23:29:24 +0530

    Day-1(13/july/2025)

--- ai-2.0/src/App.jsx
+++ ai-2.0/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState('');
+  const [result, setResult] = useState(undefined)
+
+  const payload = {
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": question
+          }
+        ]
+      }
+    ]
+  }
+
+  const askQuestion = async () => {
+    const response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDFQuEj9WlyEkFx7WylvCXHKw5RITM0IAU", {
+      method: "POST",
+      body: JSON.stringify(payload)
+    });
+
+    const data = await response.json();
+    // console.log(data.candidates[0].content.parts[0].text);
+    setResult(data.candidates[0].content.parts[0].text);
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='grid grid-cols-5 h-screen text-center'>
+      {/* Left Side Bar */}
+      <div className='col-span-1 bg-zinc-800'>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {/* Right Side Bar */}
+      <div className='col-span-4 p-4'>
+        <div className="container h-170 overflow-scroll">
+          <div className="text-white">
+            {result}
+          </div>
+        </div>
+        <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16'>
+          <input type="text" value={question} onChange={(event) => setQuestion(event.target.value)} className='w-full h-full p-3 outline-none' placeholder='Ask Me AnyThing.' />
+          <button onClick={askQuestion} className='p-2 cursor-pointer'>Ask</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
 export default App
